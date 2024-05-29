32343acc9b1256186ba4dff12812c883883e055d
kunalchahar <138198362+kunalchahar@users.noreply.github.com>
2024-05-29 11:44:15 +0530

    Toastify Component Setup

--- Rentify/frontend/src/Axios.jsx
+++ Rentify/frontend/src/Axios.jsx
@@ -1,5 +1,7 @@
 // src/axiosConfig.js
 import axios from 'axios';
+import { toast } from 'react-toastify';
+
 
 const api = axios.create({
     baseURL: 'http://localhost:4000/api', // Replace with your API base URL
@@ -8,5 +10,39 @@ const api = axios.create({
         'Content-Type': 'application/json'
     }
 });
+
+api.interceptors.request.use(
+    (config) => {
+      // Do something before request is sent
+      return config;
+    },
+    (error) => {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+  
+  // Add a response interceptor
+  api.interceptors.response.use(
+    (response) => {
+      // Any status code that lies within the range of 2xx causes this function to trigger
+      return response;
+    },
+    (error) => {
+      // Any status codes that falls outside the range of 2xx causes this function to trigger
+      if (error.response) {
+        // Server responded with a status other than 200 range
+        toast.error(`Error: ${error.response.data.message || error.response.statusText}`);
+      } else if (error.request) {
+        // Request was made but no response was received
+        toast.error('Error: No response received from server.');
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        toast.error(`Error: ${error.message}`);
+      }
+      return Promise.reject(error);
+    }
+  );
+  
 
 export default api;
--- Rentify/frontend/src/main.jsx
+++ Rentify/frontend/src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store.js'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <ToastContainer />
       </BrowserRouter>
     </Provider>
   </React.StrictMode >,
